bfc4c88810535814d8240e5aed1739ff3731c00e
Phuong Nguyen <umino.sharp@gmail.com>
2023-02-19 11:48:32 +0700

    init project

---
+++ ularm/ularmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ularmApp.swift
+//  ularm
+//
+//  Created by Phuong Nguyen Lan on 19/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ularmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
